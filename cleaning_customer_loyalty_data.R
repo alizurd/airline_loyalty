37112805b1d97647ee7bd6be3726028e7a88c7e8
alizurd <alyssabueno2014@gmail.com>
2025-02-09 02:37:50 -0500

    updating files

--- cleaning_customer_loyalty_data.R
+++ cleaning_customer_loyalty_data.R
@@ -8,6 +8,17 @@ history <- read.csv("customer_loyalty_history.csv", stringsAsFactors = FALSE)
 
 View(history)
 
+colnames(history) <- tolower(colnames(history))
+history <- history %>% rename(loyalty_number = loyalty.number)
+history <- history %>% rename(postal_code = postal.code)
+history <- history %>% rename(marital_status = marital.status)
+history <- history %>% rename(loyalty_card = loyalty.card)
+history <- history %>% rename(enrollment_type = enrollment.type)
+history <- history %>% rename(enrollment_year = enrollment.year)
+history <- history %>% rename(enrollment_month = enrollment.month)
+history <- history %>% rename(cancellation_year = cancellation.year)
+history <- history %>% rename(cancellation_month = cancellation.month)
+
 ##To Do:
 ##change column names to lowercase and snakecase
 ##change NA to 0 or NULL
