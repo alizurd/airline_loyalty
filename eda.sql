44bf908a44638e9bc8780002d632ce12b6a02608
Alyssa Bueno <149504425+alizurd@users.noreply.github.com>
2025-02-12 22:24:46 -0500

    Update eda.sql

--- eda.sql
+++ eda.sql
@@ -5,3 +5,31 @@ avg(clv) as average_clv
 from `airline.loyalty_history`
 group by city
 order by average_clv desc;
+
+--average clv by marital status
+select 
+avg(clv) as average_clv
+,avg(salary) as average_salary
+,marital_status
+from `airline.loyalty_history`
+group by
+marital_status
+order by average_clv desc;
+
+select
+count(marital_status) as total_divorced
+from `airline.loyalty_history`
+where marital_status = 'Divorced';
+--2518
+
+select
+count(marital_status) as total_married
+from `airline.loyalty_history`
+where marital_status = 'Married';
+--9735
+
+select
+count(marital_status) as total_single
+from `airline.loyalty_history`
+where marital_status = 'Single';
+--4484
