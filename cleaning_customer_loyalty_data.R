0af13b2d3cc51474b98a1fa19953e3f602e4af18
alizurd <alyssabueno2014@gmail.com>
2025-02-15 22:52:21 -0500

    abueno updated files

--- cleaning_customer_loyalty_data.R
+++ cleaning_customer_loyalty_data.R
@@ -10,14 +10,11 @@ history <- read.csv("customer_loyalty_history.csv", stringsAsFactors = FALSE)
 
 View(history)
 
-# change column names to lowercase and snakecase
-history <- clean_names(history)
+history <- clean_names(history) # change column names to lowercase and snakecase
 
-# change NA to 0 or NULL
-history[is.na(history)] <- 0
+history[is.na(history)] <- 0 # change NA to 0 or NULL
 
-# remove spaces from the postal codes
-history$postal_code <- gsub(" ", "", history$postal_code)
+history$postal_code <- gsub(" ", "", history$postal_code) # remove spaces from the postal codes
 
 # making columns numeric
 str(history)
@@ -26,6 +23,8 @@ history$salary <- as.numeric(history$salary)
 history$clv <- as.numeric(history$clv)
 history$enrollment_year <- as.numeric(history$enrollment_year)
 history$enrollment_month <- as.numeric(history$enrollment_month)
+
+history$salary <- abs(history$salary) # changing the negative salary values positive
                 
 # at this point i want to download data and explore in bigquery
 write.csv(history, "loyalty_history.csv", na = "")
@@ -34,14 +33,19 @@ write.csv(history, "loyalty_history.csv", na = "")
 check_skewness <- function(history) {
   sapply(history, function(col) if (is.numeric(col)) skewness(col, na.rm = TRUE) else NA)
 }
+skew_values <- check_skewness(history)
+print(skew_values)
 
 # clv is positively skewed >3
 # salary is positively skewed = 1.2
 history$clv <- log1p(history$clv)
 history$salary <- sqrt(log1p(history$salary))
 
-# sqrt transformation resulted warning message: NaNs
-# checking how many 0s are in column
-summary(history$salary) # there are 20 0s
+zero_rows <- sum(history$salary == 0, na.rm = TRUE)
+print(zero_rows)
+
+history <- history %>% 
+  filter(salary != 0) # filtering out rows with salary = 0
+
 
 
