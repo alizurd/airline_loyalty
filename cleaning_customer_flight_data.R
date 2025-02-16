4e431b417d1e23ad96547b2973792881f8d4b3fe
alizurd <alyssabueno2014@gmail.com>
2025-02-16 11:37:44 -0500

    updated

--- cleaning_customer_flight_data.R
+++ cleaning_customer_flight_data.R
@@ -7,13 +7,13 @@ setwd("/Users/alyssabueno/Desktop/airline_loyalty")
 flights <- read.csv("customer_flight_activity.csv", stringsAsFactors = FALSE)
 View(flights)
 
-##changing column names
+# changing column names
 flights <- clean_names(flights)
 
-##checking the data types in each column
+# checking the data types in each column
 str(flights)
 
-##making columns numeric
+# making columns numeric
 flights$loyalty_number <- as.numeric(flights$loyalty_number)
 flights$month <- as.numeric(flights$month)
 flights$total_flights <- as.numeric(flights$total_flights)
@@ -26,10 +26,10 @@ flights$year <- as.numeric(flights$year)
 flights <- flights %>%
   filter(points_redeemed <= points_accumulated) # filter out rows where points_redeemed > points_accumulated
 
-#download as csv
+# download as csv
 write.csv(flights, "flights.csv", na = "")
 
-#check for outliers
+# check for outliers
 summary(flights)
 ggplot(flights, aes(y=points_accumulated)) +
   geom_boxplot()
@@ -38,31 +38,32 @@ ggplot(flights, aes(y=points_redeemed)) +
 ggplot(flights, aes(y=dollar_cost_points_redeemed)) +
   geom_boxplot()
 
-##checking for skew
+# checking for skew
 skew_points_accumulated <- skewness(flights$points_accumulated)
 skew_points_redeemed <- skewness(flights$points_redeemed)
 skew_dollar_cost_points_redeemed <- skewness(flights$dollar_cost_points_redeemed)
 skew_distance <- skewness(flights$distance)
 skew_total_flights <- skewness(flights$skew_total_flights)
 
-##data is right skewed if > 0 
+# data is right skewed if > 0 
 
-##applying log transformation on the following:
-##points_accumulated, points_redeemed, dollar_cost_points_redeemed, distance
+# applying log transformation on the following:
+# points_accumulated, points_redeemed, dollar_cost_points_redeemed, distance
 
-##let's see what that looks like
+# let's see what that looks like
 flights$points_accumulated <- log1p(flights$points_accumulated)
 flights$points_redeemed <- log1p(flights$points_redeemed)
 flights$dollar_cost_points_redeemed <- log1p(flights$dollar_cost_points_redeemed)
 flights$distance <- log1p(flights$distance)
 
-##square root transformation on total_flights column
+# square root transformation on total_flights column
 flights$total_flights <- sqrt(log1p(flights$total_flights))
 
 View(flights)
 
-##boxplots still look funky but it's okay, that's normal
+# boxplots still look funky but it's okay, that's normal
 ggplot(flights, aes(y=points_accumulated)) +
   geom_boxplot()
 
-
+# download as csv
+write.csv(flights, "flights_transformed.csv", na = "")
--- cleaning_customer_loyalty_data.R
+++ cleaning_customer_loyalty_data.R
@@ -1,9 +1,15 @@
 install.packages("janitor")
+install.packages("lattice")
+install.packages("pROC")
+install.packages("party")
 library(janitor)
 library(moments)
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
+library(caret)
+library(pROC)
+library(party)
 
 setwd("/Users/alyssabueno/Desktop/airline_loyalty")
 history <- read.csv("customer_loyalty_history.csv", stringsAsFactors = FALSE)
@@ -14,7 +20,22 @@ history <- clean_names(history) # change column names to lowercase and snakecase
 
 history[is.na(history)] <- 0 # change NA to 0 or NULL
 
+history$salary[is.na(history$salary)] <- median(history$salary, na.rm = TRUE) # change 0 salary values to median
+
 history$postal_code <- gsub(" ", "", history$postal_code) # remove spaces from the postal codes
+
+history <- history %>%
+  mutate(country = as.factor(country),
+         province = as.factor(province),
+         city = as.factor(city),
+         postal_code = as.factor(postal_code),
+         gender = as.factor(gender),
+         education = as.factor(education),
+         marital_status = as.factor(marital_status),
+         loyalty_card = as.factor(loyalty_card),
+         enrollment_type = as.factor(enrollment_type))
+
+
 
 # making columns numeric
 str(history)
@@ -29,6 +50,18 @@ history$salary <- abs(history$salary) # changing the negative salary values posi
 # at this point i want to download data and explore in bigquery
 write.csv(history, "loyalty_history.csv", na = "")
     
+# Transformation
+
+# visual inspection
+
+# history %>% gather(clv, salary) %>%
+#   ggplot(aes(x = value)) +
+#   geom_histogram(fill = "steelblue", alpha = 0.7) +
+#   theme_minimal() + 
+#   facet_wrap(~key, scales = "free")
+
+plot(plot)
+
 # check for skew
 check_skewness <- function(history) {
   sapply(history, function(col) if (is.numeric(col)) skewness(col, na.rm = TRUE) else NA)
@@ -45,7 +78,41 @@ zero_rows <- sum(history$salary == 0, na.rm = TRUE)
 print(zero_rows)
 
 history <- history %>% 
-  filter(salary != 0) # filtering out rows with salary = 0
+  filter(salary != 0) # filtering out rows where salary = 0
+         
+View(history)
 
+# training data 70% train, 30% test
+set.seed(123)
+train_index <- createDataPartition(history$clv, p = 0.7, list = FALSE)
+train_data <- history[train_index, ]
+test_data <- history[-train_index]
+
+# train logistic regression model
+log_model <- glm(history$clv ~ history$gender + history$education + history$salary + history$marital_status + history$loyalty_card + history$salary)
+
+# model summary
+summary(log_model)
+
+# model evaluation
+test_data$pred_prob <- predict(log_model, newdata = history, type = "response")
+
+# # Convert to class labels (threshold = 0.5)
+# test_data$pred_class <- ifelse(test_data$pred_prob > 0.5, 1, 0)
+# 
+# # Confusion Matrix
+# conf_matrix <- confusionMatrix(as.factor(test_data$pred_class), test_data$purchased)
+# print(conf_matrix)
+# 
+# # AUC-ROC Curve
+# roc_obj <- roc(test_data$purchased, test_data$pred_prob)
+# auc_val <- auc(roc_obj)
+# plot(roc_obj, col = "blue", main = paste("ROC Curve (AUC =", round(auc_val, 3), ")"))
+
+# checks
+
+head(history)
+colSums(is.na(history))
+str(history)
 
 
